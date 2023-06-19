95877ca29226da7e391bef5c2d48de9386f7d8e3
Phi Hien <nguyenphihien1011@gmail.com>
2023-06-19 22:43:27 +0700

    Upload code db

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,93 @@ $(document).ready(function(){
         nextArrow:`<button type='button' class='slick-next pull-right slick-arrow'><i class="fa fa-angle-right"></i></button>`,
         
     }) ;
+})
+
+
+/* Tab */
+
+const s = document.querySelector.bind(document);
+const ss = document.querySelectorAll.bind(document);
+
+const tabs = ss('.tab-item');
+const panes = ss('.tab-pane');   
+
+const line = s('.line');
+
+tabs.forEach(function (tab ,index) {
+    const pane = panes[index];
+    tab.onclick = function () {
+        s('.tab-item.active').classList.remove('active');
+        s('.tab-pane.active').classList.remove('active');
+
+        line.style.left = tab.offsetLeft + 'px';
+        line.style.width = tab.offsetWidth + 'px';
+
+        this.classList.add('active');
+        pane.classList.add('active');
+    };
 });
 
-var btn = document.querySelector('.Login');
+/* đang ký */
 
-var form = function() {
-    window.location.href="login.html" //chuyển qua link login
+// const dk = document.querySelector('.Resigter');
+
+// dk.onclick = function () {
+//     document.querySelect('.login-container').classList.add('dky');
+// };
+
+/* ẩn hiện đặt phòng */
+const btnBuys = document.querySelectorAll('.js-home_product-btn_booking');
+const modal = document.querySelector('.js-modal')
+const closeModal = document.querySelector('.js-close-modal')
+const bodyModal = document.querySelector('.modal__body')
+
+//C1
+// btnBuys.forEach(function (btn) {
+//     btn.onclick = function () {
+//             modal.classList.add('open');
+//         }
+// })
+
+// closeModal.onclick = function() {
+//     modal.classList.remove('open');
+// }
+
+// modal.onclick = function () {
+//     modal.classList.remove('open');
+// }
+
+// bodyModal.onclick = function(e) {
+//     e.stopPropagation();
+// }
+
+//C2
+
+function showModal () {
+    modal.classList.add('open');
 }
-btn.addEventListener('click', form)
+
+function hideModal () { 
+    modal.classList.remove('open');
+}
+
+for(const btnBuy of btnBuys)  {
+    btnBuy.addEventListener('click', showModal);
+}
+
+closeModal.onclick = () => { 
+    modal.classList.remove('open');
+}
+modal.addEventListener('click', hideModal);
+
+bodyModal.addEventListener('click', function(e) { 
+    e.stopPropagation();
+});
+
+//gắn tên user tương ứng
+var username = localStorage.getItem("username");
+var nameuser = document.querySelector('.username');
+if (nameuser !== null) {
+  nameuser.innerText = username;
+}
 
